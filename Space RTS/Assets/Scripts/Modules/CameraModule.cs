f90f43b2d1a082e0638893bd2ebc169811ede177
DelaMarc <33549718+DelaMarc@users.noreply.github.com>
2022-11-04 17:22:49 +0100

    Commencedcamera movement and selection system

---
+++ Space RTS/Assets/Scripts/Modules/CameraModule.cs
@@ -0,0 +1,75 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Modules
+{
+    public class CameraModule : Module
+    {
+        [SerializeField] CinemachineFreeLook m_camera;
+        [SerializeField] float m_screenXOffset = 0, m_screenYOffset = 0;
+        float m_cameraSpeed = 20f;
+        public override void Manage()
+        {
+            base.Manage();
+            ManageMouseMovement();
+            ManageRotation();
+            //m_camera.m_XAxis.Value = Input.GetAxis("Vertical")
+        }
+
+        private void ManageRotation()
+        {
+            if (m_camera.Follow == null)
+            {
+                return;
+            }
+            if (Input.GetMouseButtonDown(1))
+            {
+                m_camera.m_XAxis.m_InputAxisName = "Mouse X";
+                m_camera.m_YAxis.m_InputAxisName = "Mouse Y";
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+                m_camera.m_XAxis.m_InputAxisName = null;
+                m_camera.m_YAxis.m_InputAxisName = null;
+            }
+            //m_camera.m_XAxis.Value = Input.GetAxis("Mouse X");
+            //m_camera.m_YAxis.Value = Input.GetAxis("Mouse Y");
+        }
+
+        private void ManageMouseMovement()
+        {
+            if (m_camera.Follow != null)
+            {
+                return;
+            }
+            //Vector3 cameraPos = m_camera.transform.position;
+            Vector3 dir = Vector3.zero;
+            if (Input.mousePosition.x <= m_screenXOffset)
+            {
+                dir += -Camera.main.transform.right;
+                //cameraPos += Vector3.left * m_cameraSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.x >= Screen.width - m_screenXOffset)
+            {
+                dir += Camera.main.transform.right;
+                //cameraPos += Vector3.right * m_cameraSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.y <= m_screenYOffset)
+            {
+                dir += -Camera.main.transform.forward;
+                //cameraPos += Vector3.back * m_cameraSpeed * Time.deltaTime;
+            }
+            if (Input.mousePosition.y >= Screen.height - m_screenYOffset)
+            {
+                dir += Camera.main.transform.forward;
+                //cameraPos += Vector3.forward * m_cameraSpeed * Time.deltaTime;
+            }
+            dir.y = 0;
+            m_camera.transform.position += dir * m_cameraSpeed * Time.deltaTime;
+            //m_camera.transform.position = cameraPos;
+        }
+
+    }
+}
---
+++ Space RTS/Assets/Scripts/Modules/SelectionModule.cs
@@ -0,0 +1,68 @@
+﻿using Cinemachine;
+using Core.Component;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Modules
+{
+    public class SelectionModule : Module
+    {
+        private Transform m_currentSelected;
+        [SerializeField] CinemachineFreeLook m_camera;
+
+        public override void Init(Entity a_entity)
+        {
+            base.Init(a_entity);
+        }
+
+        public override void Manage()
+        {
+            base.Manage();
+            if (Input.GetMouseButtonDown(0))
+            {
+                TrySelect();
+            }
+            if (Input.GetMouseButtonDown(2))
+            {
+                CenterViewOnSelected();
+            }
+        }
+
+        void CenterViewOnSelected()
+        {
+            if (m_currentSelected == null)
+            {
+                return;
+            }
+            m_camera.Follow = m_currentSelected;
+            m_camera.LookAt = m_currentSelected;
+        }
+
+        private void TrySelect()
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100f))
+            {
+                SelectableComponent selectable = hit.collider.GetComponent<SelectableComponent>();
+                if (selectable != null)
+                {
+                    Debug.Log("Selected object");
+                    m_currentSelected = selectable.transform;
+                }
+            }
+            else
+            {
+                if (m_currentSelected != null)
+                {
+                    Debug.Log("De selected object");
+                    m_currentSelected = null;
+                    m_camera.Follow = null;
+                    m_camera.LookAt = null;
+                }
+            }
+        }
+    }
+}

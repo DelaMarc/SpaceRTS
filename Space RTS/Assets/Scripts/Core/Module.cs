ead0407ad8eb424888d2cc48aa04ed7ff7787413
DelaMarc <33549718+DelaMarc@users.noreply.github.com>
2022-11-04 15:37:47 +0100

    first commit

---
+++ Space RTS/Assets/Scripts/Core/Module.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core
+{
+    public class Module : MonoBehaviour
+    {
+        public virtual void Init()
+        {
+
+        }
+
+        public virtual void Manage()
+        {
+
+        }
+
+        public virtual void Cleanup()
+        {
+
+        }
+    }
+}

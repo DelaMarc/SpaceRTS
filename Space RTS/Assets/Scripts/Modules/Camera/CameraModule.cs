a803adafb2ad1b19c9a3d5eba3d767f447c21077
DelaMarc <33549718+DelaMarc@users.noreply.github.com>
2023-04-10 19:39:01 +0200

    Updated camera follow to work on moving objects

--- Space RTS/Assets/Scripts/Modules/Camera/CameraModule.cs
+++ Space RTS/Assets/Scripts/Modules/Camera/CameraModule.cs
@@ -9,6 +9,7 @@ namespace Core.Modules.CameraManagement
 {
     public class CameraModule : Module
     {
+        private const float MAX_VERTICAL_ANGLE = 90;
         [SerializeField] float m_maxRotationAngle = 180f;
         [SerializeField] private float m_camFollowSpeed = 0.2f;
         [SerializeField] private float m_cameraLookSpeed = 2f;
@@ -55,14 +56,13 @@ namespace Core.Modules.CameraManagement
             }
             var input = Entity.Actions.Camera.RotationAxis.ReadValue<Vector2>();
             // no target selected, the camera rotates around itself
-            if (m_currentSelected == null)
+            if (Camera.Target == null)
             {
                 Vector3 rot = Vector3.zero;
                 Quaternion targetRotation;
 
                 m_lookAngle = m_lookAngle + (input.x * m_cameraLookSpeed * Time.fixedDeltaTime);
                 m_pivotAngle = Mathf.Clamp(m_pivotAngle + (input.y * m_cameraPivotSpeed * Time.fixedDeltaTime), -90f, 90f);
-                Debug.Log($"{m_lookAngle} {m_pivotAngle}");
                 rot.y = m_lookAngle;
                 rot.x = -m_pivotAngle;
                 targetRotation = Quaternion.Euler(rot);
@@ -71,38 +71,49 @@ namespace Core.Modules.CameraManagement
             }
             // camera orbits around target
             var cam = Camera.Camera;
-            Debug.Log(input);
             m_rotationY += input.x * m_cameraLookSpeed * Time.fixedDeltaTime;
             m_rotationX -= input.y * m_cameraPivotSpeed * Time.fixedDeltaTime;
-            m_rotationX = Mathf.Clamp(m_rotationX, -90, 90);
+            m_rotationX = Mathf.Clamp(m_rotationX, -MAX_VERTICAL_ANGLE, MAX_VERTICAL_ANGLE);
             Quaternion rotation = Quaternion.Euler(m_rotationX, m_rotationY, 0);
             // Calcule la position de la caméra en fonction de la rotation et de la distance par rapport à l'objet cible
-            Vector3 position = rotation * new Vector3(0, 0, -m_targetDistance) + m_currentSelected.transform.position;
+            Vector3 position = rotation * new Vector3(0, 0, -m_targetDistance) + Camera.Target.transform.position;
             // Applique la rotation et la position de la caméra
             cam.transform.transform.rotation = rotation;
-            cam.transform.transform.position = position;            
+            cam.transform.transform.position = position;
         }
 
         private void FollowTarget()
         {
-            if (Camera.Target == null)
+            if (Camera.Target == null || m_rotating)
             {
                 return;
             }
             Vector3 dest = Camera.Target.transform.position - m_zoomDir;
-            Vector3 pos = Camera.Camera.transform.position;
-            float dist = (pos - dest).sqrMagnitude;
-            if (dist < 0.01f)
+            float distance = Vector3.Distance(dest, Camera.Camera.transform.position);
+            float refDist = m_zoomCount == 1 ? 0.01f : m_zoomMax;
+            //smooth follow target
+            if (distance > refDist)
             {
-                //m_zoomCount = 0;
-                m_camFollowVelocity = Vector3.zero;
-                return;
+                Vector3 pos = Camera.Camera.transform.position;
+                float dist = (pos - dest).sqrMagnitude;
+                if (dist < 0.01f)
+                {
+                    m_camFollowVelocity = Vector3.zero;
+                    return;
+                }
+                var newPos = Vector3.SmoothDamp(pos,
+                    dest,
+                    ref m_camFollowVelocity,
+                    m_camFollowSpeed);
+                Camera.Camera.transform.position = newPos;
             }
-            var newPos = Vector3.SmoothDamp(pos,
-                dest,
-                ref m_camFollowVelocity,
-                m_camFollowSpeed);
-            Camera.Camera.transform.position = newPos;
+            //lock camera to target if we are close enough
+            else
+            {
+                Camera.Camera.transform.position = dest;
+            }
+            Debug.Log($"Follow dist from dest point {(Camera.Camera.transform.position - dest).sqrMagnitude} | {m_zoomMax}");
+            Debug.DrawLine(Camera.Camera.transform.position, dest, Color.black);
         }
 
         #region Events
@@ -119,16 +130,13 @@ namespace Core.Modules.CameraManagement
                 {
                     m_currentSelected = selectable;
                     m_zoomCount = 0;
-                    Debug.Log($"Selected object {m_currentSelected.name}");
                 }
             }
             else
             {
                 if (Camera.Target != null)
                 {
-                    Debug.Log("De selected object");
                     m_currentSelected = null;
-                    Camera.Target = null;
                 }
             }
         }
@@ -159,9 +167,15 @@ namespace Core.Modules.CameraManagement
             m_rotating = obj.performed;
             if (m_rotating && m_currentSelected != null)
             {
-                Camera.Target = null;
-                m_zoomCount = 0;
                 m_targetDistance = Vector3.Distance(Camera.Camera.transform.position, m_currentSelected.transform.position);
+                Camera.Target = m_currentSelected?.transform;
+            }
+            // Rotation released, set zoom settings to first state
+            else
+            {
+                m_zoomMax = Vector3.Distance(Camera.Target.position, Camera.Camera.transform.position);
+                m_zoomDir = Camera.Camera.transform.forward * m_zoomMax;
+                m_zoomCount = 1;
             }
         }
         #endregion
